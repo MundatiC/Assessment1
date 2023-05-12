8384d120cce85d14c9d3d7efd3bbc3ad1befda72
MundatiC <calebngethe230@gmail.com>
2023-05-12 10:21:15 +0300

    remove console log from function

--- equal_eight.js
+++ equal_eight.js
@@ -1,21 +1,22 @@
 function equalEight(a,b){
     
     if(a === 8){
-        console.log(`${a} is equal to eight`);
+        return`${a} is equal to eight`;
     } else if(b === 8){
-        console.log(`${b} is equal to eight`);
+        return`${b} is equal to eight`;
     }
     else if(Math.abs(a-b) === 8 || Math.abs(a+b) === 8){
-        console.log(`The sum or difference of ${a} and ${b} is equal to 8`);
+        return `The sum or difference of ${a} and ${b} is equal to 8`;
     }
     else{
-        console.log(`Neither of the numbers are equal to eight and neither are their sums or products`);
+        return `Neither of the numbers are equal to eight and neither are their sums or products`;
     }    
     }
 
 
-equalEight(3,8);
-equalEight(8,8);
-equalEight(4,4);
-equalEight(3,3);  
-equalEight(16,-8);  
+console.log(equalEight(3,8)); 
+console.log(equalEight(8,8)); 
+console.log(equalEight(4,4)); 
+console.log(equalEight(3,3));
+console.log(equalEight(16,-8));  
+  
