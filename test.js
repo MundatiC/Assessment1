a0db66dc99da978e45282ac7d6dc94e1c1b214e5
MundatiC <calebngethe230@gmail.com>
2023-05-12 11:04:05 +0300

    add program that displays operator overloading of the '+' opeartor

---
+++ test.js
@@ -0,0 +1,4 @@
+let sum = 5+ 5+"5";
+
+console.log(sum);
+console.log(`The result will be ${sum} since Javascript evaluates expressions from let to right and thus string 5 will be concatenated to the sum of the first expresion. This is because the first '+' is  the addition operator since its used with numbers and the second '+'  concatenated the strings because when '+' is used with strings it performs concatenation.`);
